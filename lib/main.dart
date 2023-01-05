3f4a40ec49a5c3d2e9292ea4212b2dca656a6980
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-01-05 18:25:35 -0300

    App do Curso do Balda.io Alcoool e Gasolina

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home.page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Álcool ou Gasolina',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: HomePage(),
+    );
+  }
+}
